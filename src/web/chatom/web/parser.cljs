4356141954f8d06c86983dd91a23a5bd55dbe835
Jed <jed.clinger@gmail.com>
2015-12-04 15:38:05 -0800

    Make parser recursive on :db key

--- src/web/chatom/web/parser.cljs
+++ src/web/chatom/web/parser.cljs
@@ -5,8 +5,8 @@
 (defmulti read om/dispatch)
 
 (defmethod read :default
-  [{:keys [state query ast]} key params]
-  (let [st @state]
+  [{:keys [state db query ast]} key params]
+  (let [st (or db @state)]
     (if-let [[_ v] (find st key)]
       (let [val (if query (om/db->tree query v st) v)]
         {:value val})
@@ -18,15 +18,9 @@
         current-page-ref (get-in st [:app/routing :data :app/current-page])
         current-page (get-in st current-page-ref)
         page-query (get query (:id current-page))
-        ;; FIXME: need better logic to compute the remote query
-        ;; seems like make default read recursive if a :tree/:db in env
-        ;; and use that for values, then just call parser in remote
-        ;; mode to get the remote query. Currently the :remote query
-        ;; returned will contain the full query except the :id or any
-        ;; keys that happen to be in both the page and the top level.
-        remote-query (into [] (remove #(= :id %)) page-query)
-        value (om/db->tree page-query (get-in st current-page) st)
-        remote-query (parser {:state state} remote-query :remote)]
+        env {:state state :db current-page}
+        value (parser env page-query)
+        remote-query (parser env page-query :remote)]
     {:value [value]
      :remote (om/query->ast remote-query)}))
 
